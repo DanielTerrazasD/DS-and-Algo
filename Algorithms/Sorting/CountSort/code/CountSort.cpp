220adb5cb2ab5bfd607abf37abb613999a425ea2
DanielTerrazasD <62128296+DanielTerrazasD@users.noreply.github.com>
2021-02-14 18:45:19 -0700

    Adding Count Sort
    Adding files that implement count sort algorithm

---
+++ Algorithms/Sorting/CountSort/code/CountSort.cpp
@@ -0,0 +1,115 @@
+/*****************************************************************************/
+/** Copyright (c) 2021, Daniel Terrazas                                     **/
+/** ----------------------------------------------------------------------- **/
+/**                                Algorithms                               **/
+/** ----------------------------------------------------------------------- **/
+/** FILE: CountSort.cpp                                                     **/
+/** ----------------------------------------------------------------------- **/
+/** DESCRIPTION:                                                            **/
+/** CountSort.cpp description.                                              **/
+/** ----------------------------------------------------------------------- **/
+/**                                                                         **/
+/** Permission is hereby granted, free of charge, to any person obtaining a **/
+/** copy of this software and associated documentation files                **/
+/** (the "Software") to deal in the Software without restriction, including **/
+/** without limitation the rights to use, copy, modify, merge, publish,     **/
+/** distribute, sublicense, and/or sell copies of the Software, and to      **/
+/** permit persons to whom the Software is furnished to do so, subject to   **/
+/** the following conditions:                                               **/
+/**                                                                         **/
+/** The above copyright notice and this permission notice shall be included **/
+/** in all copies or substantial portions of the Software.                  **/
+/**                                                                         **/
+/** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS **/
+/** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF              **/
+/** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  **/
+/** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY    **/
+/** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    **/
+/** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       **/
+/** SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  **/
+/**                                                                         **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                               Preprocessors                             **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                  Includes                               **/
+/*****************************************************************************/
+
+#include "CountSort.hpp"
+
+/*****************************************************************************/
+/**                                   Macros                                **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                 Data Types                              **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                 Constants                               **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                 Variables                               **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                  Classes                                **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                            Function Prototypes                          **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                            Function Definitions                         **/
+/*****************************************************************************/
+
+/********************************* Count Sort ********************************/
+void CountSort(std::vector<int> &array)
+{
+    int max = 0;
+    int j, k;
+    int size = ((int)array.size());
+    std::vector<int> count_array;
+
+    if (size <= 1) return;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+
+    count_array.resize(max + 1);
+
+    for (int i = 0; i < size; i++)
+    {
+        count_array[array[i]]++;
+    }
+
+    j = 0; k = 0;
+    while (k < size)
+    {
+        if (count_array[j] != 0)
+        {
+            array[k] = j;
+            count_array[j]--;
+            k++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+}
+
+
+/*****************************************************************************/
+/**                                END OF FILE                              **/
+/*****************************************************************************/
