b4c33fe7749dbd5ac57ba5739f3f84b79dab0b8f
DanielTerrazasD <62128296+DanielTerrazasD@users.noreply.github.com>
2021-02-20 00:05:43 -0700

    Adding Shell Sort
    Adding files that implement shell sort algorithm

---
+++ Algorithms/Sorting/ShellSort/code/ShellSort.cpp
@@ -0,0 +1,98 @@
+/*****************************************************************************/
+/** Copyright (c) 2021, Daniel Terrazas                                     **/
+/** ----------------------------------------------------------------------- **/
+/**                                Algorithms                               **/
+/** ----------------------------------------------------------------------- **/
+/** FILE: ShellSort.cpp                                                     **/
+/** ----------------------------------------------------------------------- **/
+/** DESCRIPTION:                                                            **/
+/** ShellSort.cpp description.                                              **/
+/** ----------------------------------------------------------------------- **/
+/**                                                                         **/
+/** Permission is hereby granted, free of charge, to any person obtaining a **/
+/** copy of this software and associated documentation files                **/
+/** (the "Software") to deal in the Software without restriction, including **/
+/** without limitation the rights to use, copy, modify, merge, publish,     **/
+/** distribute, sublicense, and/or sell copies of the Software, and to      **/
+/** permit persons to whom the Software is furnished to do so, subject to   **/
+/** the following conditions:                                               **/
+/**                                                                         **/
+/** The above copyright notice and this permission notice shall be included **/
+/** in all copies or substantial portions of the Software.                  **/
+/**                                                                         **/
+/** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS **/
+/** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF              **/
+/** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  **/
+/** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY    **/
+/** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    **/
+/** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       **/
+/** SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  **/
+/**                                                                         **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                               Preprocessors                             **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                  Includes                               **/
+/*****************************************************************************/
+
+#include "ShellSort.hpp"
+
+/*****************************************************************************/
+/**                                   Macros                                **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                 Data Types                              **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                 Constants                               **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                 Variables                               **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                                  Classes                                **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                            Function Prototypes                          **/
+/*****************************************************************************/
+
+/*****************************************************************************/
+/**                            Function Definitions                         **/
+/*****************************************************************************/
+
+/********************************* Shell Sort ********************************/
+void ShellSort(std::vector<int> &array)
+{
+    int tmp;
+    int j;
+    int size = static_cast<int>(array.size());
+
+    if (size <= 1) return;
+
+    for (int gap =  size / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < size; i++)
+        {
+            tmp = array[i];
+
+            for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+            {
+                array[j] = array[j - gap];
+            }
+
+            array[j] = tmp;
+        }
+    }
+}
+
+/*****************************************************************************/
+/**                                END OF FILE                              **/
+/*****************************************************************************/
